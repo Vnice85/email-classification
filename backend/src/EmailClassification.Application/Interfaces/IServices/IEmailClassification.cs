017aeae6b17d555d429175a6d8dd0daeb73b3a88
Vnice85 <ducvan85204@gmail.com>
2025-05-20 16:47:43 +0700

    Implement mock classification service

---
+++ backend/src/EmailClassification.Application/Interfaces/IServices/IEmailClassification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailClassification.Application.Interfaces.IServices
+{
+    public interface IClassificationService
+    {
+        Task<string> IdentifyLabel(string emailContent);
+    }
+}
---
+++ backend/src/EmailClassification.Infrastructure/Implement/EmailClassification.cs
@@ -0,0 +1,19 @@
+﻿using EmailClassification.Application.Interfaces.IServices;
+
+namespace EmailClassification.Infrastructure.Implement
+{
+    public class ClassificationService : IClassificationService
+    {
+
+        public async Task<string> IdentifyLabel(string emailContent)
+        {
+           
+            // fake result
+            await Task.Delay(100); 
+            var label = new[] { "SPAM", "UNDEFINE", "NORMAL", "PHISING"};
+            var random = new Random();
+            var value = random.Next(0, label.Count());
+            return label[value].ToUpper();
+        }
+    }
+}
