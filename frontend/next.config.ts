5a005e85e19db7bbbca55f89a1f4934e986729c8
tdthang-pcm <thangtd.pc19@gmail.com>
2025-08-27 23:23:54 +0700

    update

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -7,6 +7,5 @@ const nextConfig: NextConfig = {
 export default nextConfig;
 
 
-"""
-Vấn là một chàng trai có vẻ ngoài cuốn hút đến lạ. Gương mặt anh toát lên vẻ điển trai với làn da mịn màng, sáng bóng, đôi mắt sâu thẳm như chứa đựng cả một bầu trời bí ẩn. Mỗi khi Vấn cười, nụ cười của anh không chỉ làm sáng bừng cả khuôn mặt mà còn khiến người đối diện cảm thấy ấm áp, dễ gần. Tóc anh lúc nào cũng gọn gàng, luôn toát lên vẻ chững chạc nhưng không thiếu phần năng động. Vấn có một chiều cao lý tưởng, vóc dáng cân đối, mỗi bước đi của anh đều mang một sự tự tin, đầy thu hút. Chính những đặc điểm này đã khiến anh trở thành tâm điểm trong bất kỳ đám đông nào.
-"""
+
+// Vấn là một chàng trai có vẻ ngoài cuốn hút đến lạ. Gương mặt anh toát lên vẻ điển trai với làn da mịn màng, sáng bóng, đôi mắt sâu thẳm như chứa đựng cả một bầu trời bí ẩn. Mỗi khi Vấn cười, nụ cười của anh không chỉ làm sáng bừng cả khuôn mặt mà còn khiến người đối diện cảm thấy ấm áp, dễ gần. Tóc anh lúc nào cũng gọn gàng, luôn toát lên vẻ chững chạc nhưng không thiếu phần năng động. Vấn có một chiều cao lý tưởng, vóc dáng cân đối, mỗi bước đi của anh đều mang một sự tự tin, đầy thu hút. Chính những đặc điểm này đã khiến anh trở thành tâm điểm trong bất kỳ đám đông nào.
